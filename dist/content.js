f8c8b32b41ff364643c115afa07fef00a2aa1f11
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2022-12-24 23:22:06 +0200

    Add build

---
+++ dist/content.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.createElement("style");e.textContent='\nytd-grid-video-renderer:has(a[href*="shorts"]),\nytd-rich-section-renderer:has(ytd-rich-shelf-renderer[is-shorts]) {\n  display: none !important;\n}\n',(document.head||document.documentElement).appendChild(e)})();
