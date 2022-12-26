e5f67170d0101c92a4e641fd4992a33d81375f4d
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2022-12-27 00:05:56 +0200

    Fix selectors

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,12 +1,24 @@
-/* We inject style */
+// We create the element
 const styleElement: HTMLStyleElement = document.createElement("style");
+// CSS Selectors for hiding elements
+// Thumbnails on Home and Subscriptions tabs
+const homeAndSubIndividual = 'ytd-grid-video-renderer:has(a[href*="shorts"])';
+// Groups of Shorts on Home and Subscriptions tabs
+const homeAndSubGroup =
+  "ytd-rich-section-renderer:has(ytd-rich-shelf-renderer[is-shorts])";
+// Thumbnails appearing in search and on "related videos"
+const searchAndRelated = 'ytd-video-renderer:has(a[href*="shorts"])';
+// Groups appearing in search
+const searchGroup = 'ytd-reel-shelf-renderer:has(path[d*="M17"])';
+// We add the styles to the element
 styleElement.textContent = `
-ytd-grid-video-renderer:has(a[href*="shorts"]),
-ytd-video-renderer:has(a[href*="shorts"]),
-ytd-rich-section-renderer:has(ytd-rich-shelf-renderer[is-shorts]),
-ytd-item-section-renderer:has(yt-horizontal-list-renderer):has(a[href*="shorts"])
+${homeAndSubIndividual},
+${homeAndSubGroup},
+${searchAndRelated},
+${searchGroup}
 {
   display: none !important;
 }
 `;
+// We add the element to the DOM
 (document.head || document.documentElement).appendChild(styleElement);
